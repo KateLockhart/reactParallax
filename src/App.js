82c001079e6d8376b65baa48e26a90ca113ec886
KateLockhart <katelockhart707@gmail.com>
2022-08-31 09:46:26 -0400

    Added img files and filled jsx for landing section.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,33 @@ import "./styles/main_styles.css";
 function App() {
   return (
     <div className="main_container">
-      <div className="section1"></div>
+      <div className="section1">
+        <img
+          src="./images/space.png"
+          alt="Star filled dark night sky."
+          className="space"
+        />
+        <img
+          src="./images/earth.png"
+          alt="Planet earth with a tilt showing Europe and Africa."
+          className="earth"
+        />
+        <img
+          src="./images/planet.png"
+          alt="A planet resembling a simplified Saturn."
+          className="planet"
+        />
+        <img
+          src="./images/rocket.png"
+          alt="A playful representation of a space rocket."
+          className="rocket"
+        />
+        <img
+          src="./images/satellite.png"
+          alt="A space satellite in orbit."
+          className="satellite"
+        />
+      </div>
 
       <div className="section2">
         <svg
@@ -17,6 +43,11 @@ function App() {
           <path d="M0,100 C65,93 76,10 100,100" />
         </svg>
 
+        <div className="section2_container">
+          <div className="left_side"></div>
+          <div className="right_side"></div>
+        </div>
+
         <svg
           className="svg_curve_bottom"
           xmlns="http://www.w3.org/2000/svg"
@@ -28,7 +59,13 @@ function App() {
         </svg>
       </div>
 
-      <div className="section3"></div>
+      <div className="section3">
+        <div className="contacts">
+          <div className="contact_section"></div>
+          <div className="contact_section"></div>
+          <div className="contact_section"></div>
+        </div>
+      </div>
     </div>
   );
 }
