e2fa753d16129f7b2764adfcecd50d58d4121954
KateLockhart <katelockhart707@gmail.com>
2022-09-01 15:14:24 -0400

    Custom button component completed with jsx elements and functionality.

--- src/components/MyButton.jsx
+++ src/components/MyButton.jsx
@@ -1,9 +1,23 @@
+import { useState } from 'react';
+import '../styles/myButton_styles.css';
 
-function MyButton() {
-    
+function MyButton(props) {
+    const [clicked, setClicked] = useState(false);
+
+    function button_clicked() {
+        setClicked(true);
+
+        setTimeout(() => {
+            setClicked(false);
+        }, 2000);
+    }
+
     return (
-        <div>
-
+        <div className='button_container'>
+            <div onClick={button_clicked} className={`my_button ${clicked ? 'clicked' : null}`}>
+                <span>{props.name}</span>
+            </div>
+            <img src='./images/rocket.png' alt='A cartoon style illustrative rocket ship blasting off to the upper right of the webpage.' className={`button_image ${clicked ? 'launch_rocket' : null}`}/>
         </div>
     )
 };
